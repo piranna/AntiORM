8d499b4348c9c695518eeed876ceb73bd41caacc
piranna <jleganes@qdqmedia.com>
2012-01-16 11:53:36 +0100

    Moved DictObj, DictObj_factory and ChunkConverted to __init__
    This is in preparation to remove them back to PirannaFS

--- antiorm/AntiORM.py
+++ antiorm/AntiORM.py
@@ -14,38 +14,6 @@ from sqlparse.filters import Tokens2Unicode
 from sql2 import Compact, GetColumns, GetLimit, IsType
 
 
-class DictObj(dict):
-    def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(name)
-
-    def __setattr__(self, name, value):
-        try:
-            self[name] = value
-        except KeyError:
-            raise AttributeError(name)
-
-
-def DictObj_factory(cursor, row):
-    d = DictObj()
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-
-def ChunkConverted(chunk):
-    """[Hack] None objects get converted to 'None' while SQLite queries
-    expect 'NULL' instead. This function return a newly dict with
-    all None objects converted to 'NULL' valued strings.
-    """
-    d = {}
-    for key, value in chunk.iteritems():
-        d[key] = "NULL" if value == None else value
-    return d
-
-
 class AntiORM():
     '''
     classdocs
--- antiorm/__init__.py
+++ antiorm/__init__.py
@@ -1,1 +1,33 @@
-from AntiORM import AntiORM, ChunkConverted, DictObj, DictObj_factory
+from AntiORM import AntiORM
+
+
+class DictObj(dict):
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        try:
+            self[name] = value
+        except KeyError:
+            raise AttributeError(name)
+
+
+def DictObj_factory(cursor, row):
+    d = DictObj()
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def ChunkConverted(chunk):
+    """[Hack] None objects get converted to 'None' while SQLite queries
+    expect 'NULL' instead. This function return a newly dict with
+    all None objects converted to 'NULL' valued strings.
+    """
+    d = {}
+    for key, value in chunk.iteritems():
+        d[key] = "NULL" if value == None else value
+    return d
