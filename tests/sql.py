622e37251acdf07e437ea3136daca7165216ee80
piranna <jleganes@qdqmedia.com>
2012-02-14 12:34:13 +0100

    Enabled INCLUDE statement for Compact tests

--- tests/sql.py
+++ tests/sql.py
@@ -18,7 +18,7 @@ class Test_SQL(TestCase):
     sql = """-- type: script
             -- return: integer
 
-            INCLUDE "Direntry.make.sql";
+            INCLUDE "_Make_DirEntry.sql";
 
             INSERT INTO directories(inode)
                             VALUES(:inode)
@@ -62,9 +62,9 @@ LIMIT 1"""
 
 class Test_Compact(Test_SQL):
     def test_compact1(self):
-        self.assertEqual(Tokens2Unicode(Compact(self.sql)),
-            'INCLUDE "Direntry.make.sql";INSERT INTO directories(inode) VALUES'
-            '(:inode) LIMIT 1')
+        self.assertEqual(Tokens2Unicode(Compact(self.sql, 'samples_sql')),
+            'INSERT INTO dir_entries(type) VALUES(:type);INSERT INTO '
+            'directories(inode) VALUES(:inode) LIMIT 1')
 
     def test_compact2(self):
         self.assertEqual(Tokens2Unicode(Compact(self.sql2)),
