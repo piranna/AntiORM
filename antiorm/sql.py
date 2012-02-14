60b0763ee29e9fa7c8e0dcbb7fe2447160fe9a81
piranna <jleganes@qdqmedia.com>
2012-02-14 11:29:43 +0100

    Removed unused check code

--- antiorm/sql.py
+++ antiorm/sql.py
@@ -5,12 +5,12 @@ Created on 01/08/2011
 '''
 
 
-from sqlparse.filters import ColumnsSelect, IncludeStatement, Limit
-from sqlparse.filters import StripComments, SerializerUnicode
-from sqlparse.filters import StripWhitespaceFilter
-from sqlparse.lexer import tokenize
+from sqlparse.filters  import ColumnsSelect, IncludeStatement, Limit
+from sqlparse.filters  import StripComments, SerializerUnicode
+from sqlparse.filters  import StripWhitespaceFilter
+from sqlparse.lexer    import tokenize
 from sqlparse.pipeline import Pipeline
-from sqlparse.tokens import Keyword, Whitespace
+from sqlparse.tokens   import Keyword, Whitespace
 
 
 def Compact(sql, includePath="sql"):
@@ -57,37 +57,3 @@ class IsType():
         for token_type, value in stream:
             if token_type in Whitespace: continue
             return token_type in Keyword and value == self.type
-
-
-if __name__ == '__main__':
-    import sqlparse
-
-    from sqlparse.engine import FilterStack
-    from sqlparse.filters import KeywordCaseFilter
-
-
-#    print sqlparse.split(sql3)
-
-#    print sqlparse.parse(sql2)[0].tokens
-#    print sqlparse.parse(sql)[0].tokens[1].tokens
-#    print sqlparse.parse(sql)[0].tokens[1].tokens[0]
-
-#    print sqlparse.parse(sql)[0].tokens[1].tokens[2].tokens
-#    print sqlparse.parse(sql)[0].tokens[1].tokens[2].tokens[0]
-
-#    print sqlparse.parse(sql)[1].tokens
-
-
-#    stack = FilterStack()
-
-#    stack.preprocess.append(KeywordCaseFilter('upper')) # uppercase keywords too
-#    stack.stmtprocess.append(StripCommentsFilter())
-#    stack.preprocess.append(Compact())
-#    stack.preprocess.append(Get_Comments())
-
-#    stack.preprocess.append(IncludeStatement("sql"))
-#    stack.preprocess.append(StripComments())
-#    stack.stmtprocess.append(StripWhitespaceFilter())
-#    stack.postprocess.append(SerializerUnicode())
-#
-#    print ''.join(stack.run(sql))
