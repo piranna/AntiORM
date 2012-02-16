a313dbeea2e418bd14b5bdbce4ca15bf33f7fd11
piranna <jleganes@qdqmedia.com>
2012-02-16 16:15:41 +0100

    Improved base class tests

--- tests/run.py
+++ tests/run.py
@@ -11,7 +11,8 @@ from antiorm.backends.sqlite import Sqlite
 from antiorm.utils           import DictObj_factory, named2pyformat
 
 
-class MainTestCase(TestCase):
+class TestAntiORM(TestCase):
+    "Test for the AntiORM base driver"
     @classmethod
     def setUpClass(cls):
         cls.connection = connect(":memory:")
@@ -27,16 +28,26 @@ class MainTestCase(TestCase):
     def setUp(self):
         self.engine = AntiORM(self.connection, "./samples_sql")
 
-    def test_sample_method_exists(self):
-        self.engine.test_simple_insert(key="hola")
-        attr = getattr(self.engine, 'test_simple_insert', None)
+    def test_method_notparsed(self):
+        with self.assertRaises(AttributeError):
+            self.engine.notparsed()
+
+    def test_method_exists(self):
+        attr = getattr(self.engine, 'test_statement_INSERT_single', None)
         self.assertNotEqual(attr, None)
 
+    def test_statement_INSERT_single_1(self):
+        rowid = self.engine.test_statement_INSERT_single(key="hola")
+
+        self.assertIsNotNone(rowid)
+
+    def test_statement_INSERT_single_2(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM test_table")
         result = cursor.fetchall()
 
         self.assertEqual(len(result), 1)
+        self.assertEqual(len(result[0]), 1)
         self.assertEqual(result[0][0], u'hola')
 
 
@@ -56,28 +67,6 @@ class TestUtils(TestCase):
         self.assertEqual(named2pyformat("asdf"), "asdf")
         self.assertEqual(named2pyformat("a :formated word"),
                          "a %(formated)s word")
-
-
-class TestAntiORM(TestCase):
-    "Test for the AntiORM base driver"
-    @classmethod
-    def setUpClass(cls):
-        cls.connection = connect(":memory:")
-        cursor = cls.connection.cursor()
-        cursor.execute("CREATE TABLE test_table (key TEXT);")
-        cursor.close()
-        cls.connection.commit()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.connection.close()
-
-    def setUp(self):
-        self.engine = AntiORM(self.connection, "./samples_sql")
-
-    def test_notparsed(self):
-        with self.assertRaises(AttributeError):
-            self.engine.notparsed()
 
 
 class TestSqlite(TestAntiORM):
