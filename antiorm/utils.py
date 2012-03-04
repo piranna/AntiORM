b55df1e5bddfabba0b974484063b31b9fbe93125
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-03-04 22:29:14 +0100

    Deprecated TupleObj in benefict of Namedtuple_factory

--- antiorm/utils.py
+++ antiorm/utils.py
@@ -1,22 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from re import sub
+from collections import namedtuple
+from re          import sub
 
 
-def TupleObj_factory(cursor, row):
-    "Create a TupleObj from a DB-API 2.0 cursor description and its values"
-    t = TupleObj(cell for cell in row)
-    for idx, col in enumerate(cursor.description):
-        setattr(t, col[0], t[idx])
-    return t
+def Namedtuple_factory(cursor, row):
+    "Create a namedtuple from a DB-API 2.0 cursor description and its values"
+
+    return namedtuple('namedtuple',
+                      (col[0] for col in cursor.description))(*row)
 
 
 def named2pyformat(sql):
     "Convert from 'named' paramstyle format to Python string 'pyformat' format"
     return sub(":\w+", lambda m: "%%(%s)s" % m.group(0)[1:], sql)
-
-
-class TupleObj(tuple):
-    "Tuple that allow access to its elements as object attributes"
-    pass
-
--- tests/utils.py
+++ tests/utils.py
@@ -5,7 +5,7 @@ from unittest import main, TestCase
 import sys
 sys.path.insert(0, '..')
 
-from antiorm.utils import TupleObj_factory, named2pyformat
+from antiorm.utils import Namedtuple_factory, named2pyformat
 
 
 class FakeCursor:
@@ -18,18 +18,19 @@ class TestUtils(TestCase):
 
     fakecursor = FakeCursor(('a', 'b', 'c'))
 
-    def test_TupleObj_factory(self):
-        tupleobj = TupleObj_factory(self.fakecursor, ('x', 'y', 'z'))
+    def test_Namedtuple_factory(self):
+        namedtuple = Namedtuple_factory(self.fakecursor, ('x', 'y', 'z'))
 
-        self.assertEqual(tupleobj, ('x', 'y', 'z'))
+        self.assertEqual(namedtuple, ('x', 'y', 'z'))
+        self.assertEqual(namedtuple._asdict(), {'a': 'x', 'b': 'y', 'c': 'z'})
 
-        self.assertEqual(tupleobj[0], 'x')
-        self.assertEqual(tupleobj[1], 'y')
-        self.assertEqual(tupleobj[2], 'z')
+        self.assertEqual(namedtuple[0], 'x')
+        self.assertEqual(namedtuple[1], 'y')
+        self.assertEqual(namedtuple[2], 'z')
 
-        self.assertEqual(tupleobj.a, 'x')
-        self.assertEqual(tupleobj.b, 'y')
-        self.assertEqual(tupleobj.c, 'z')
+        self.assertEqual(namedtuple.a, 'x')
+        self.assertEqual(namedtuple.b, 'y')
+        self.assertEqual(namedtuple.c, 'z')
 
     def test_named2pyformat(self):
         self.assertEqual(named2pyformat(""), "")
