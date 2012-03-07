312e2df1d82ec0b8045110435790d9904f4f4e46
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-03-07 22:05:24 +0100

    Fixed tests (althought they are really ugly...)

--- tests/backends.py
+++ tests/backends.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from os.path  import abspath, dirname, join
 from sqlite3  import connect
-from unittest import main
+from unittest import main, TestCase
 
 import sys
 sys.path.insert(0, '..')
@@ -11,15 +12,26 @@ from antiorm.backends.generic import Generic
 from antiorm.backends.sqlite  import Sqlite
 from antiorm.utils            import Namedtuple_factory
 
-from base import Test
+from base import Basic
+from base import StatementINSERTSingle, StatementINSERTMultiple
+from base import OneStatement_value, OneStatement_register, OneStatement_table
+from base import MultipleStatement
 
 
-class TestAPSW(Test):
+class TestAPSW(TestCase,
+               Basic,
+               StatementINSERTSingle, StatementINSERTMultiple,
+               OneStatement_value, OneStatement_register, OneStatement_table,
+               MultipleStatement):
     "Test for the AntiORM APSW driver"
     def setUp(self):
+        self.dir_path = join(abspath(dirname(__file__)), 'samples_sql')
+
         self.connection = connect(":memory:")
 
-        Test.setUp(self)
+        for base in self.__class__.__bases__:
+            if hasattr(base, 'setUp'):
+                base.setUp(self)
 
         self.engine = APSW(self.connection, self.dir_path)
         self.engine.row_factory = Namedtuple_factory
@@ -28,12 +40,21 @@ class TestAPSW(Test):
         pass
 
 
-class TestGeneric(Test):
+class TestGeneric(TestCase,
+                  Basic,
+                  StatementINSERTSingle, StatementINSERTMultiple,
+                  OneStatement_value, OneStatement_register, OneStatement_table,
+                  MultipleStatement):
     "Test for the AntiORM generic driver"
     def setUp(self):
+        self.dir_path = join(abspath(dirname(__file__)), 'samples_sql')
+
         self.connection = connect(":memory:")
 
-        Test.setUp(self)
+        for base in self.__class__.__bases__:
+            if hasattr(base, 'setUp'):
+                print repr(base)
+                base.setUp(self)
 
         self.engine = Generic(self.connection, self.dir_path)
 
@@ -41,12 +62,20 @@ class TestGeneric(Test):
         self.connection.close()
 
 
-class TestSqlite(Test):
+class TestSqlite(TestCase,
+                 Basic,
+                 StatementINSERTSingle, StatementINSERTMultiple,
+                 OneStatement_value, OneStatement_register, OneStatement_table,
+                 MultipleStatement):
     "Test for the AntiORM SQLite driver"
     def setUp(self):
+        self.dir_path = join(abspath(dirname(__file__)), 'samples_sql')
+
         self.connection = connect(":memory:")
 
-        Test.setUp(self)
+        for base in self.__class__.__bases__:
+            if hasattr(base, 'setUp'):
+                base.setUp(self)
 
         self.engine = Sqlite(self.connection, self.dir_path)
         self.engine.row_factory = Namedtuple_factory
--- tests/base.py
+++ tests/base.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from os.path  import abspath, dirname, join
-from unittest import main, TestCase
-
-import sys
-sys.path.insert(0, '..')
-
 
 class Basic:
     def test_method_notparsed(self):
@@ -24,12 +18,11 @@ class StatementINSERTSingle:
         cursor.close()
         self.connection.commit()
 
-    def test_statement_INSERT_single_1(self):
+    def test_statement_INSERT_single(self):
         rowid = self.engine.test_statement_INSERT_single(key="hola")
 
         self.assertIsNotNone(rowid)
 
-    def test_statement_INSERT_single_2(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM test_statement_INSERT_single")
         result = cursor.fetchall()
@@ -38,21 +31,20 @@ class StatementINSERTSingle:
         self.assertEqual(len(result[0]), 1)
         self.assertEqual(result[0][0], u'hola')
 
-    def test_statement_INSERT_single_dict_1(self):
+    def test_statement_INSERT_single_dict(self):
         rowid = self.engine.test_statement_INSERT_single({'key': "adios"})
 
         self.assertIsNotNone(rowid)
 
-    def test_statement_INSERT_single_dict_2(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM test_statement_INSERT_single")
         result = cursor.fetchall()
 
-        self.assertEqual(len(result), 2)
-        self.assertEqual(len(result[1]), 1)
-        self.assertEqual(result[1][0], u'adios')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(len(result[0]), 1)
+        self.assertEqual(result[0][0], u'adios')
 
-    def test_statement_INSERT_single_list_1(self):
+    def test_statement_INSERT_single_list(self):
         rowid = self.engine.test_statement_INSERT_single([{'key': 'a'},
                                                           {'key': 'b'}])
 
@@ -60,16 +52,15 @@ class StatementINSERTSingle:
         self.assertIsNotNone(rowid[0])
         self.assertIsNotNone(rowid[1])
 
-    def test_statement_INSERT_single_list_2(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM test_statement_INSERT_single")
         result = cursor.fetchall()
 
-        self.assertEqual(len(result), 4)
-        self.assertEqual(len(result[2]), 1)
-        self.assertEqual(result[2][0], u'a')
-        self.assertEqual(len(result[3]), 1)
-        self.assertEqual(result[3][0], u'b')
+        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result[0]), 1)
+        self.assertEqual(result[0][0], u'a')
+        self.assertEqual(len(result[1]), 1)
+        self.assertEqual(result[1][0], u'b')
 
 
 class StatementINSERTMultiple:
@@ -83,12 +74,11 @@ class StatementINSERTMultiple:
         cursor.close()
         self.connection.commit()
 
-    def test_statement_INSERT_multiple_1(self):
+    def test_statement_INSERT_multiple(self):
         rowid = self.engine.test_statement_INSERT_multiple(key='a')
 
         self.assertIsNotNone(rowid)
 
-    def test_statement_INSERT_multiple_2(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM test_statement_INSERT_multiple")
         result = cursor.fetchall()
@@ -98,23 +88,22 @@ class StatementINSERTMultiple:
         self.assertEqual(result[0][0], u'a')
         self.assertEqual(result[0][0], result[0][1])
 
-    def test_statement_INSERT_multiple_dict_1(self):
+    def test_statement_INSERT_multiple_dict(self):
         rowid = self.engine.test_statement_INSERT_multiple({'key': 'b'})
 
         self.assertIsNotNone(rowid)
 
-    def test_statement_INSERT_multiple_dict_2(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM test_statement_INSERT_multiple")
         result = cursor.fetchall()
 
-        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result), 1)
 
-        self.assertEqual(len(result[1]), 2)
-        self.assertEqual(result[1][0], u'b')
-        self.assertEqual(result[1][0], result[1][1])
+        self.assertEqual(len(result[0]), 2)
+        self.assertEqual(result[0][0], u'b')
+        self.assertEqual(result[0][0], result[0][1])
 
-    def test_statement_INSERT_multiple_list_1(self):
+    def test_statement_INSERT_multiple_list(self):
         rowid = self.engine.test_statement_INSERT_multiple([{'key': 'c'},
                                                             {'key': 'd'}])
 
@@ -122,20 +111,19 @@ class StatementINSERTMultiple:
         self.assertIsNotNone(rowid[0])
         self.assertIsNotNone(rowid[1])
 
-    def test_statement_INSERT_multiple_list_2(self):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM test_statement_INSERT_multiple")
         result = cursor.fetchall()
 
-        self.assertEqual(len(result), 4)
+        self.assertEqual(len(result), 2)
 
-        self.assertEqual(len(result[2]), 2)
-        self.assertEqual(result[2][0], u'c')
-        self.assertEqual(result[2][0], result[2][1])
+        self.assertEqual(len(result[0]), 2)
+        self.assertEqual(result[0][0], u'c')
+        self.assertEqual(result[0][0], result[0][1])
 
-        self.assertEqual(len(result[3]), 2)
-        self.assertEqual(result[3][0], u'd')
-        self.assertEqual(result[3][0], result[3][1])
+        self.assertEqual(len(result[1]), 2)
+        self.assertEqual(result[1][0], u'd')
+        self.assertEqual(result[1][0], result[1][1])
 
 
 class OneStatement_value:
@@ -204,22 +192,3 @@ class MultipleStatement:
         result = list(cursor.execute("SELECT * FROM test_multiple_statement"))
 
         self.assertListEqual(result, [(u'c',)])
-
-
-class Test(TestCase, Basic,
-           StatementINSERTSingle, StatementINSERTMultiple,
-           OneStatement_value, OneStatement_register, OneStatement_table,
-           MultipleStatement):
-    def __init__(self, methodName='runTest'):
-        TestCase.__init__(self, methodName)
-
-        self.dir_path = join(abspath(dirname(__file__)), 'samples_sql')
-
-    def setUp(self):
-        for base in self.__class__.__bases__:
-            if hasattr(base, 'setUp'):
-                base.setUp(self)
-
-
-if __name__ == "__main__":
-    main()
