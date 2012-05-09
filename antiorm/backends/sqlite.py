2e898541974535b9db8b01b3a5e7c6c9b7a6c289
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-05-09 21:33:07 +0200

    Added support for row_factory on SQLite backend

--- antiorm/backends/sqlite.py
+++ antiorm/backends/sqlite.py
@@ -25,6 +25,14 @@ class Sqlite(Base):
 
         self.tx_manager = db_conn
 
+    @property
+    def row_factory(self):
+        return self.connection.row_factory
+
+    @row_factory.setter
+    def row_factory(self, value):
+        self.connection.row_factory = value
+
     def _multiple_statement_standard__dict(self, stmts):
         sql = named2pyformat(''.join(stmts))
 
