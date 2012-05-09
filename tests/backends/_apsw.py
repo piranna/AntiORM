42d17276a1156a563d2d2d6c22d1f90ea1f5a6a2
Jesús Leganés Combarro <piranna@gmail.com>
2012-05-09 07:55:59 +0000

    Created TestFactory base class

--- tests/backends/_apsw.py
+++ tests/backends/_apsw.py
@@ -15,63 +15,57 @@ from antiorm.utils            import Namedtuple_factory, driver_factory
 from base import Base
 
 
+class TestFactory(Base):
+    def setUp(self):
+        self.engine.row_factory = Namedtuple_factory
+
+        Base.setUp(self)
+
+    def test_driver_factory(self):
+        self.assertIsInstance(self.engine, APSW)
+
+
 @skip
 #@skipIf('apsw' not in sys.modules, "APSW not installed on the system")
-class Driver(Base, TestCase):
+class Driver(TestFactory, TestCase):
     "Test for the AntiORM APSW driver"
     def setUp(self):
         self.connection = Connection(":memory:")
         self.engine = driver_factory(self.connection, self.dir_path)
-        self.engine.row_factory = Namedtuple_factory
 
-        Base.setUp(self)
-
-    def test_driver_factory(self):
-        self.assertIsInstance(self.engine, APSW)
+        TestFactory.setUp(self)
 
 
 @skip
 #@skipIf('apsw' not in sys.modules, "APSW not installed on the system")
-class Driver__ByPass(Base, TestCase):
+class Driver__ByPass(TestFactory, TestCase):
     "Test for the AntiORM APSW driver"
     def setUp(self):
         self.connection = Connection(":memory:")
         self.engine = driver_factory(self.connection, self.dir_path, True)
-        self.engine.row_factory = Namedtuple_factory
 
-        Base.setUp(self)
-
-    def test_driver_factory(self):
-        self.assertIsInstance(self.engine, APSW)
+        TestFactory.setUp(self)
 
 
 @skipIf('apsw' not in sys.modules, "APSW not installed on the system")
-class Driver__LazyLoading(Base, TestCase):
+class Driver__LazyLoading(TestFactory, TestCase):
     "Test for the AntiORM APSW driver"
     def setUp(self):
         self.connection = Connection(":memory:")
         self.engine = driver_factory(self.connection, self.dir_path, False, True)
-        self.engine.row_factory = Namedtuple_factory
 
-        Base.setUp(self)
-
-    def test_driver_factory(self):
-        self.assertIsInstance(self.engine, APSW)
+        TestFactory.setUp(self)
 
 
 @skip
 #@skipIf('apsw' not in sys.modules, "APSW not installed on the system")
-class Driver__ByPass__LazyLoading(Base, TestCase):
+class Driver__ByPass__LazyLoading(TestFactory, TestCase):
     "Test for the AntiORM APSW driver"
     def setUp(self):
         self.connection = Connection(":memory:")
         self.engine = driver_factory(self.connection, self.dir_path, True, True)
-        self.engine.row_factory = Namedtuple_factory
 
-        Base.setUp(self)
-
-    def test_driver_factory(self):
-        self.assertIsInstance(self.engine, APSW)
+        TestFactory.setUp(self)
 
 
 @skip
