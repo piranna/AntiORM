02925c842747ab1ca7c9b2640105b135f7dc9eb3
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-05-26 01:08:20 +0200

    Added tests for PostgreSQL database with Generica backend (not working)

---
+++ tests/backends/postgresql.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from unittest import skip, skipIf, main, TestCase
+except ImportError:
+    from unittest2 import skip, skipIf, main, TestCase
+
+from psycopg2 import connect
+
+import sys
+sys.path.insert(0, '..')
+
+from antiorm.backends.generic import Generic
+
+from base import Base
+
+
+@skipIf('psycopg2' not in sys.modules, "psycopg2 not installed on the system")
+class GenericDriver(Base, TestCase):
+    "Test for the AntiORM generic driver"
+    def setUp(self):
+        self.test_name = 'test_' + self.__class__.__name__
+
+        self.connection = connect("")
+        cursor = self.connection.cursor()
+        cursor.execute('CREATE DATABASE IF NOT EXISTS %s' % self.test_name)
+        cursor.execute('USE %s' % self.test_name)
+
+        self.engine = Generic(self.connection, self.dir_path)
+
+        Base.setUp(self)
+
+    def tearDown(self):
+        cursor = self.connection.cursor()
+        cursor.execute('DROP DATABASE %s' % self.test_name)
+
+
+@skip
+#@skipIf('psycopg2' not in sys.modules, "psycopg2 not installed on the system")
+class GenericDriver__ByPass(Base, TestCase):
+    "Test for the AntiORM generic driver"
+    def setUp(self):
+        self.connection = connect(db=":memory:")
+        self.engine = Generic(self.connection, self.dir_path, True)
+
+        Base.setUp(self)
+
+
+@skip
+#@skipIf('psycopg2' not in sys.modules, "psycopg2 not installed on the system")
+class GenericDriver__LazyLoading(Base, TestCase):
+    "Test for the AntiORM generic driver"
+    def setUp(self):
+        self.connection = connect(db=":memory:")
+        self.engine = Generic(self.connection, self.dir_path, False, True)
+
+        Base.setUp(self)
+
+
+@skip
+#@skipIf('psycopg2' not in sys.modules, "psycopg2 not installed on the system")
+class GenericDriver__ByPass__LazyLoading(Base, TestCase):
+    "Test for the AntiORM generic driver"
+    def setUp(self):
+        self.connection = connect(db=":memory:")
+        self.engine = Generic(self.connection, self.dir_path, True, True)
+
+        Base.setUp(self)
+
+
+if __name__ == "__main__":
+    main()
