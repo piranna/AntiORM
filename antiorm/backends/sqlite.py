f3ca15e0af513980aa1d3f8113252f1555cbe00e
Jesús Leganés Combarro "Piranna <jesus.lc@vaelsys.com>
2012-06-04 15:31:16 +0200

    Finished code documentation

--- antiorm/backends/sqlite.py
+++ antiorm/backends/sqlite.py
@@ -1,4 +1,6 @@
 '''
+AntiORM specialized backend for PySQLite
+
 Created on 20/01/2012
 
 @author: piranna
@@ -11,7 +13,9 @@ from antiorm.utils import named2pyformat
 
 
 def quote_sql(value):
-    "Quote correctly the SQL string according to SQL standard"
+    """
+    Quote correctly the SQL string according to SQL standard
+    """
     # None - Null
     if isinstance(value, NoneType):
         return 'NULL'
@@ -29,7 +33,9 @@ def quote_sql(value):
 
 
 def _quote_sql_fromdict(d):
-    "Quote for SQL all the items on a dict"
+    """
+    Quote for SQL all the items on a dict
+    """
     result = {}
     for key, value in d.iteritems():
         result[key] = quote_sql(value)
@@ -37,15 +43,19 @@ def _quote_sql_fromdict(d):
 
 
 class Sqlite(Base):
-    "SQLite driver for AntiORM"
-
+    """
+    SQLite driver for AntiORM
+    """
     def __init__(self, db_conn, dir_path=None, bypass_types=False, lazy=False):
-        """Constructor
+        """
+        Constructor
 
         @param db_conn: connection of the database
         @type db_conn: DB-API 2.0 database connection
         @param dir_path: path of the dir with files from where to load SQL code
         @type dir_path: string
+        @param bypass_types: set if types should be bypassed on calling
+        @type bypass_types: boolean
         @param lazy: set if SQL code at dir_path should be lazy loaded
         @type lazy: boolean
         """
@@ -54,9 +64,27 @@ class Sqlite(Base):
         self.tx_manager = db_conn
 
     def _multiple_statement_standard__dict(self, stmts):
+        """
+        Factory for functions with multiple statements by dict
+
+        @param stmts: the list of sql queries
+        @type stmts: iterable of strings
+
+        @return: the optimized function
+        @rtype: method function
+        """
         sql = named2pyformat(''.join(stmts))
 
         def _wrapped_method(self, kwargs):
+            """
+            Exec the statement and return the result of executes
+
+            @param kwargs: the keyword arguments of the query
+            @type kwargs: dict
+
+            @return: the result of executes
+            @rtype: list
+            """
             kwargs = _quote_sql_fromdict(kwargs)
 
             with self.tx_manager as conn:
@@ -67,9 +95,27 @@ class Sqlite(Base):
         return _wrapped_method
 
     def _multiple_statement_standard__list(self, stmts):
+        """
+        Factory for functions with multiple statements by list
+
+        @param stmts: the list of sql queries
+        @type stmts: iterable of strings
+
+        @return: the optimized function
+        @rtype: method function
+        """
         sql = named2pyformat(''.join(stmts))
 
         def _wrapped_method(self, list_kwargs):
+            """
+            Exec the statement and return the a list of lists of results
+
+            @param list_kwargs: a list with the keyword arguments of the query
+            @type list_kwargs: list of dicts
+
+            @return: the list of lists of results of execute
+            @rtype: list of lists
+            """
             list_kwargs = map(_quote_sql_fromdict, list_kwargs)
 
             result = []
