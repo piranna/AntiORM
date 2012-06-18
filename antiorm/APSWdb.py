8feaa72fbef0b2284ebcd3895cb193163925ae2a
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-06-18 22:58:51 +0200

    Added stub for APSWdb

---
+++ antiorm/APSWdb.py
@@ -0,0 +1,20 @@
+'''
+APSWdb - A DB-API 2.0 wrapper for APSW inspired by MySQLdb module
+
+Created on 18/06/2012
+
+@author: piranna
+'''
+
+from apsw import
+
+
+class APSWdb:
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
