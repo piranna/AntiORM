40182df9e4348aae488def555e2df02a18f56f6b
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-06-29 00:45:18 +0200

    Added stubs

--- antiorm/APSWdb.py
+++ antiorm/APSWdb.py
@@ -6,10 +6,21 @@ Created on 18/06/2012
 @author: piranna
 '''
 
-from apsw import
+#from apsw import
+import apsw
 
 
-class APSWdb:
+apilevel = '2.0'
+threadsafety =
+paramstyle = 'qmark'
+
+
+
+def connect():
+    pass
+
+
+class Connection:
     '''
     classdocs
     '''
@@ -18,3 +29,51 @@ class APSWdb:
         '''
         Constructor
         '''
+
+    def close(self):
+        pass
+
+    def commit(self):
+        pass
+
+    def rollback(self):
+        pass
+
+    def cursor(self):
+        pass
+
+
+class Cursor:
+    arraysize = 1
+
+    @property
+    def description(self):
+        pass
+
+    @property
+    def rowcount(self):
+        pass
+
+    def close(self):
+        pass
+
+    def execute(operation, parameters=None):
+        pass
+
+    def executemany(operation, seq_of_parameters):
+        pass
+
+    def fetchone(self):
+        pass
+
+    def fetchmany(size=cursor.arraysize):
+        pass
+
+    def fetchall(self):
+        pass
+
+    def setinputsizes(sizes):
+        pass
+
+    def setoutputsize(size, column=None):
+        pass
